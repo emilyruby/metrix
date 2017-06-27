9de746073e31243f0d92e6dbede17a50d607d944
emilyruby <zcabeme@ucl.ac.uk>
2017-06-27 17:05:57 +0100

    changed react app to vue
    just for prototype development

--- metrix/api/views.py
+++ metrix/api/views.py
@@ -1,5 +1,19 @@
-from django.http import HttpResponse
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from tutorial.quickstart.serializers import UserSerializer, GroupSerializer
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
